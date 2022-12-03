2d18700cb5cebf69e082293b2430e2dfa4d71087
NguyenTriQuan <nguyentriquan99@gmail.com>
2022-12-03 08:49:20 +0700

    wn

--- networks/wn_net.py
+++ networks/wn_net.py
@@ -300,7 +300,7 @@ class ResNet(nn.Module):
         return nn.Sequential(*layers)
 
     def normalize(self):
-        for m in self.WN[1:-1]:
+        for m in self.WN:
             m.normalize()
 
     def forward(self, x):
