b716dddd4da29a7855d4d0bd1b92449cd6413734
NguyenTriQuan <nguyentriquan99@gmail.com>
2023-01-26 16:30:41 +0700

    fse

--- layers/wn_layer.py
+++ layers/wn_layer.py
@@ -98,10 +98,10 @@ class WeightNormLinear(_WeightNormLayer):
         super(WeightNormLinear, self).__init__(in_features, out_features, bias, activation, norm_type)
 
         self.weight = nn.Parameter(torch.Tensor(self.out_features, self.in_features).to(device))
-        self.initialize()
         self.norm_dim = (1)
         self.norm_view = (-1, 1)
         self.ks = 1
+        self.initialize()
 
     def forward(self, x):  
         out = self.activation(x)
@@ -138,10 +138,10 @@ class WeightNormConv2D(_WeightNormConvNd):
                                             stride, padding, dilation, False, _pair(0), groups, bias, activation, norm_type)
 
         self.weight = nn.Parameter(torch.Tensor(self.out_features, self.in_features // self.groups, *self.kernel_size).to(device))
-        self.initialize()
         self.norm_dim = (1, 2, 3)
         self.norm_view = (-1, 1, 1, 1)
         self.ks = np.prod(self.kernel_size)
+        self.initialize()
 
     def forward(self, x): 
         out = self.activation(x)
